cb9b6e4b88460e40cbffe5bf0b75dee20c9df091
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2019-10-23 17:58:09 +0000

    fixed security issue

--- security/src/backdoor.php
+++ security/src/backdoor.php
@@ -2,7 +2,9 @@
     if(empty($_SESSION["auth"]) || $_SESSION["auth"] != 'true') {
         echo "<p class='login'>L O G I N </p><p class='login'>R E Q U I R E D</p>";
     } else {
-        $sandbox = '/var/www/html/sandbox/' . md5("xehos" . $_GET['sandboxid']);
+        $sandbox = '/var/www/html/sandbox/';
+        @mkdir($sandbox);
+        $sandbox = $sandbox . md5("xehos" . $_GET['sandboxid']);
         @mkdir($sandbox);
         @chdir($sandbox);
 
