d3010f27370c4e7b1f177dd7c5a81f097103e4a2
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2019-10-29 00:44:50 -0400

    fixed theo and ai grader

--- ai/spellchecker/grader.py
+++ ai/spellchecker/grader.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python3
 import json
+import sys
 
 f = open("answers.json", "r")
 answers = json.loads(f.read())
 f.close()
 
-f = open("answers.json", "r")
+f = open(sys.argv[1], "r")
 candidate_answers = json.loads(f.read())
 f.close()
+print(candidate_answers)
 
 good_answers = 0
 bad_answers = []
 
 for (word, answer) in candidate_answers.items():
-    if word in answers and answer in answers[word]:
-        a = answers[word]
-        if (isinstance(a, str) and answer == a) or answer in a:
-            good_answers += 1
-        else:
-            bad_answers += [(word, answer)]
+    if word not in answers:
+        continue
+
+    a = answers[word]
+    if (isinstance(a, str) and answer == a) or (not isinstance(a, str) and answer in a):
+        good_answers += 1
+    else:
+        bad_answers += [(word, answer)]
 
 print(bad_answers)
 print(f"{good_answers}/{len(answers.values())}")
